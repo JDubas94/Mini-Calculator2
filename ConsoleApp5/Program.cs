386273853b637c2d2654c1454e0ea1aefa75d06e
Julia Dubas <uliadubas94@gmail.com>
2022-07-30 13:22:36 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FirstNumber, SecondNumber;
+
+            string decision;
+
+            Console.WriteLine("Enter the first number:");
+
+            FirstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+
+            SecondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter operation: '+', '-', '*', '/' ");
+
+            decision = Console.ReadLine();
+
+            switch (decision)
+            {
+                case "+":
+                    Console.WriteLine(FirstNumber + SecondNumber);
+                    break;
+                case "-":
+                    Console.WriteLine(FirstNumber - SecondNumber);
+                    break;
+                case "*":
+                    Console.WriteLine(FirstNumber * SecondNumber);
+                    break;
+                case "/":
+                    Console.WriteLine(FirstNumber / SecondNumber);
+                    break;
+            }
+        }
+    }
+}
